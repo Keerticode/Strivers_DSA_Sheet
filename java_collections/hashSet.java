30f1e05aacd35889d0d7c93365a5f9f000cf2fd7
Keerticode <keertihiregoudar13@gmail.com>
2026-05-13 19:26:38 +0530

    add these files

--- java_collections/hashSet.java
+++ java_collections/hashSet.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class hashSet {
     public static void main(String[] args) {
-        
+
+        Set<Integer> set = new HashSet<>();
+        set.add(32);
+        set.add(12);
+        set.add(10);
+        set.add(18);
+        set.add(15);
+
+        System.out.println(set);
+        set.remove(32);
+        System.out.println(set);
+
+        System.out.println(set.contains(12));
+        System.out.println(set.isEmpty());
+        System.out.println(set.size());
+
+        set.clear();
+
+        System.out.println(set);
+
     }
 }
