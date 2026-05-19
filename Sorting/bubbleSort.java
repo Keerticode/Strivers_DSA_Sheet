59edcdc23830ab6de4986e68fbadad96c78624c5
Keerticode <keertihiregoudar13@gmail.com>
2026-05-19 18:37:30 +0530

    add files

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,17 +1,46 @@
-
 import java.util.Scanner;
 
 public class bubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        System.out.println("Enter array : ");
-        int[] arr = new int[n];
 
+        System.out.println("Enter length of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array : ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
+        for (int i = 0; i < n - 1; i++) {
+
+            int didSwap = 0;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    didSwap = 1;
+                }
+            }
+
+            if (didSwap == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Sorted array of given array using bubble sort is : ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
+
+// if array is already sorted then to reduce the time complexity and optimize
+// the program we add the didSwap.
---
+++ Sorting/insertionSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array : ");
+        int n = sc.nextInt();
+
+        int nums[] = new int[n];
+        System.out.print("Enter array : ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && nums[j - 1] > nums[j]) {
+                int temp = nums[j - 1];
+                nums[j - 1] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+
+        
+        System.out.println("Sorted array of given array using insertion sort is : ");
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
