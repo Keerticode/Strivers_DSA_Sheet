99419c2842b5c3b1e82bc661e1237056da1edcec
Keerticode <keertihiregoudar13@gmail.com>
2026-06-18 12:16:41 +0530

    add files

--- Arrays/prblm15.java
+++ Arrays/prblm15.java
@@ -26,3 +26,40 @@ public class prblm15 {
 }
 */
 
+public class prblm15 {
+    public static void main(String[] args) {
+
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
+        int n = nums.length;
+
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+
+            else if (nums[mid] == 2) {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
